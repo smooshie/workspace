37714eadff5ddd8037a50ff2fac3da2b328fd5ed
smooshie <timere@gmail.com>
2013-01-21 17:36:16 +0200

    meow

--- Work/createInversion.py
+++ Work/createInversion.py
@@ -4,18 +4,18 @@ Created on 11.1.2013
 @author: lisagawr
 '''
 
-def rc(input):
+def rc(reversable):
     complement = { "A" : "T", "T" : "A", "C" : "G", "G" : "C"}
     result = [] 
-    for i in reversed(input): 
+    for i in reversed(reversable): 
         result.append(complement[i])
     
     return ''.join(result)
 
 def main():
     sequence = '''GGATTACAGGTGTGAGCCACTGCGCCTGGCCTGTTGAACTTTTAACATACTTCCCTGTGCTCTAGCCACAGCCACACTGGTTCCCGTTAGCTAAATTTTTAACCATACAGTATTGTCATTTGCATGCTGTAGTTAAGAGCTTCCAGAGGTTACATGCACTAGATCTTAACAGCTTAGCCCTCATGAATAACATGTTATCTTCCACCATCTCAGGGAAGCAGTGATACCGTGGAGTGAGAAAAGTGCATTCTCTCAGGATTTCTGCCGTGTTTTCATGCTGAGGTGTGACAGAGTATTTCC'''
-    reversed = rc(sequence)
-    print reversed
+    reversd = rc(sequence)
+    print reversd
                     
 
 main()

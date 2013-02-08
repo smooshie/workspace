1902d2d792152caa1431fa2a73df57dc893defaa
smooshie <timere@gmail.com>
2013-02-08 16:43:38 +0200

    muu

--- Work/regFinder.py
+++ Work/regFinder.py
@@ -28,7 +28,40 @@ class RegionFinder(object) :
             regions.append((boundaries[i], boundaries[i+1]))
 
         return regions
-
+    
+    
 if __name__ == '__main__' :
-    rf = RegionFinder(10)
-    print rf.get_regions([0,5,6,8,10,35,36,40,45,48,78,79,80])
+    readflags = {}
+    
+    with open('paired_unmapped.sam', 'r') as matesw:
+        for line in matesw:
+            line.rstrip()
+            if line[0] != "@":
+                mateparts = line.split()
+                readflags[mateparts[0]] = mateparts[7] #name:pos of next
+        
+    positions = []
+    for entry in readflags.iterkeys():
+        positions.append(int(readflags[entry]))
+    
+    rf = RegionFinder(100)
+    regions = rf.get_regions(positions)
+    
+    allreads = open("sorted_mapped12.sam", "r")#java -jar ../../picard-tools-1.84/SortSam.jar I=mapped12.sam O=sorted_mapped12.sam SO$
+
+    for line in allreads:
+        if line[0] != "@":
+            allparts = line.split()
+            for each in regions:
+                boundaries = each[0],each[1]
+                filename = "mapped12_"+str(boundaries[0])+"-"+str(boundaries[1])+".sam"
+                writeto = open(filename, "w")
+                if int(boundaries[0]) <= int(allparts[3]) and int(allparts[3]) <= int(boundaries[1] and int(allparts[3]) != 0):
+                        print boundaries[0], boundaries[1], allparts[3]
+                        writeto.write(line)
+            if allparts[3] > boundaries[1]:
+                continue
+
+                
+
+                    
